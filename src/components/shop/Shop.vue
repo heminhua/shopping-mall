1f1be4b63be586c82a7820add7c4b3b66333872b
heminhua <15218482830@163.com>
2018-03-08 21:57:01 +0800

    路由配置

---
+++ src/components/shop/Shop.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+       <!-- 头部公共内容 -->
+       <app-header></app-header>
+       <!-- /头部公共内容 -->
+       <!-- 主要内容 -->
+        <router-view></router-view>
+       <!-- /主要内容 -->
+       <!-- 公共尾部 -->
+       <app-footer></app-footer>
+       <!-- /公共尾部 -->
+    </div>
+</template>
+
+<script>
+import appHeader from '../subcom/Header.vue'
+import appFooter from '../subcom/Footer.vue'
+export default {
+  components: {
+      appHeader:appHeader,
+      appFooter:appFooter,
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+import Login from '../components/account/Login.vue'
+import Shop from '../components/shop/Shop.vue'
+import GoodsList from '../components/shop/goods/List.vue'
+import GoodsDetail from '../components/shop/goods/detail.vue'
+import OrderComplete from '../components/shop/order/Complete.vue'
+import OrderPay from '../components/shop/order/Pay.vue'
+import Ordersite from '../components/shop/order/site.vue'
+import Shopcart from '../components/shop/shopcart/Shopcart.vue'
+
+//配置商品路由
+let goods =[
+{name:'goodsList',path:'goods/list',component:GoodsList},
+{name:'goodsDetail',path:'goods/detail',component:GoodsDetail},
+];
+//配置订单路由
+let order=[
+  {name:'orderComplete',path:'order/complete',component:OrderComplete},
+  {name:'orderPay',path:'order/complete',component:OrderPay},
+  {name:'ordersite',path:'order/complete',component:Ordersite},
+]
+//配置购物车路由
+let shopcart=[
+  {name:'shopcart',path:'shopcart',component:Shopcart}
+]
+
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+   {name:'login',path:'/login',component:Login},
+   {name:'shop',path:'/',component:Shop,children:[...goods,...order,...shopcart]},
+   
   ]
 })
